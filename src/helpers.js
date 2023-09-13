14ed8b07a9f2bf3c9762f2b25b9dc28d699da0ad
John Campbell <campbj49@gmail.com>
2023-09-13 12:08:19 -0400

    remove() actually works; index printing messages

--- src/helpers.js
+++ src/helpers.js
@@ -15,10 +15,9 @@ function choice(items){
  * @returns first instance of findItem in items if it exists, undefined if not
  */
 function remove(items, findItem){
-    for(item of items){
-        if(item === findItem) {
-            //delete item;
-            return findItem;
+    for(let i = 0; i< items.length; i++){
+        if(items[i] === findItem) {
+            return items.splice(i,1);
         }
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import {choice, remove} from "./helpers";
 import fruits from "./foods";
 
-console.log("Something is happening here");
-choice();
+let rndFruit = choice(fruits);
+console.log(`I'd like one ${rndFruit}, please}` );
+console.log(`Here you go:${rndFruit}`);
+remove(fruits,rndFruit);
+//fruits = ["hee"];
+console.log("Delicious! May I have another?");
+console.log(`I'm sorry we're all out. We have ${fruits.length} left.`)
